093c7d0a2480e66a4e50231555d3b83bbf3da9f9
Feffernoose <matt.penny82@gmail.com>
2022-07-19 13:28:54 -0400

    Added user profile picture upload

--- app/src/main/java/com/example/moviematchv2/UpdateAccount.java
+++ app/src/main/java/com/example/moviematchv2/UpdateAccount.java
@@ -1,37 +1,54 @@
 package com.example.moviematchv2;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class UpdateAccount extends AppCompatActivity {
 
     private EditText nameET, phoneET, passwordET;
     private ImageView profilePic;
     private ImageButton updateAccount;
-
+    private Button uploadImg;
     private FirebaseAuth mAuth;
     private DatabaseReference userDb;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    private Uri filepath;
 
     private String userId, name, phone, password, profileUri;
 
-    private Uri resultUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +60,15 @@ public class UpdateAccount extends AppCompatActivity {
         phoneET = findViewById(R.id.updatePhone);
         passwordET = findViewById(R.id.updatePassword);
         updateAccount = findViewById(R.id.sendRequestButton);
+        uploadImg = findViewById(R.id.uploadImg);                
         profilePic = findViewById(R.id.profileImage);
-
+        
 
         mAuth = FirebaseAuth.getInstance();
         userId = mAuth.getCurrentUser().getUid();
         userDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
-
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
         profilePic.setOnClickListener(view -> {
             Intent intent = new Intent(Intent.ACTION_PICK);
@@ -62,7 +81,49 @@ public class UpdateAccount extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), LobbyAccount.class);
             startActivity(intent);
         });
+        
+        uploadImg.setOnClickListener(view -> {
+            uploadImage();
+        });
+    
     }
+
+    private void uploadImage() {
+        if (filepath != null) {
+            // Code for showing progressDialog while uploading
+            ProgressDialog progressDialog
+                    = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            // Defining the child of storageReference
+            StorageReference ref = storageReference.child("images/" + UUID.randomUUID().toString());
+
+            // adding listeners on upload or failure of image
+            ref.putFile(filepath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {// Image uploaded successfull Dismiss dialog
+                            progressDialog.dismiss();
+                            Toast.makeText(UpdateAccount.this, "Image Uploaded!!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {  // Error, Image not uploaded
+                            progressDialog.dismiss();
+                            Toast.makeText(UpdateAccount.this, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {// Progress Listener for loading percentage on the dialog box
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            progressDialog.setMessage("Uploaded " + (int) progress + "%");
+                        }
+                    });
+        }
+    }
+
 
     // Update user information from Users who do not use Google or Facebook
     private void saveUserInformation() {
@@ -89,20 +150,54 @@ public class UpdateAccount extends AppCompatActivity {
         userInfo.put("name", name);
         userInfo.put("phone", phone);
         userInfo.put("password", password);
-
         userDb.updateChildren(userInfo);
 
+    /*    if(resultUri != null) {
+            StorageReference filepath = FirebaseStorage.getInstance().getReference().child("profileImages").child(userId);
+            Bitmap bitmap = null;
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+            byte[] data = baos.toByteArray();
+            UploadTask uploadTask = filepath.putBytes(data);
+            uploadTask.addOnFailureListener(e -> { finish(); });
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                //  Uri downloadUrl = taskSnapshot.getDownloadUrl(); // this is red and doesn't work for some reason.
+                    Map userInfo = new HashMap();
+                    userInfo.put("profileImageUrl",filepath);
+                    userDb.updateChildren(userInfo);
+                    finish();
+                }
+            });
+        } else {
+            finish();
+        }*/
         Toast.makeText(UpdateAccount.this, "Information updated successfully.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1 && resultCode == Activity.RESULT_OK) {
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
             final Uri imageUri = data.getData();
-            resultUri = imageUri;
-            profilePic.setImageURI(resultUri);
+            filepath = imageUri;
+            profilePic.setImageURI(filepath);
 
+            filepath = data.getData();
+
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
+                    profilePic.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
-}
